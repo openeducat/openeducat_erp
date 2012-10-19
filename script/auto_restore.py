2ff643265c8a2fbcaa97682f66a9c2ebd4e341c0
Grishma Shukla <ruchir@techreceptives.com>
2012-10-19 06:10:26 +0000

    add script

---
+++ script/auto_restore.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import xmlrpclib
+import time
+
+from ConfigParser import ConfigParser
+
+print "Srcipt Starts",time.strftime("%Y-%m-%d %H:%M:%S")
+config = ConfigParser()
+config.read('config.conf')
+mapping = dict(config.items('openerp'))
+print "FFFFFFFFFFFFFF",mapping
+sock = xmlrpclib.ServerProxy(mapping['url'])
+sock.drop(mapping['admin_password'],mapping['db'])
+print "DROP"
+sock.create_database(mapping['admin_password'], mapping['db'], True, 'en_US', mapping['new_password'])
+print "CREATE"
+sock = xmlrpclib.ServerProxy(mapping['url'].replace("db","object"))
+print sock
+
+module_ids = sock.execute(mapping['db'], 1, mapping['new_password'], 'ir.module.module', 'search', [('name','=',mapping['module_name'])])
+print "$$$$$$$$$$$",module_ids
+sock.execute(mapping['db'], 1, mapping['new_password'], 'ir.module.module', 'button_immediate_install', module_ids,)
+print "Script Ends",time.strftime("%Y-%m-%d %H:%M:%S")
+
+
+
