1743e201780f3a5311dfc5d36d3097dca3251c5c
Vijayant <vijayant@techreceptives.com>
2013-09-13 11:31:14 +0530

    [FIX] version number

--- openeducat_erp/__openerp__.py
+++ openeducat_erp/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'OpenEduCat ERP',
-    'version': '1.0',
+    'version': '1.3',
     'category': 'Openerp Education',
     "sequence": 3,
     'summary': 'Manage Students, Faculties and Education Institute',
