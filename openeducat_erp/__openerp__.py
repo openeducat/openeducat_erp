3944264d5026f2673a93c21c2d19e80f40049ab1
Parthiv Patel <parthivgls@gmail.com>
2014-12-18 00:55:16 -0800

    [FIX] modified version number for v8

--- openeducat_erp/__openerp__.py
+++ openeducat_erp/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'OpenEduCat ERP',
-    'version': '1.3.1',
+    'version': '1.0',
     'category': 'Openerp Education',
     "sequence": 3,
     'summary': 'Manage Students, Faculties and Education Institute',
