0219a18e9b14474a8eea79466c5bacf413d8f9a9
Parthiv Patel <parthivgls@gmail.com>
2015-01-09 09:19:12 +0530

    Update __openerp__.py

--- openeducat_erp/__openerp__.py
+++ openeducat_erp/__openerp__.py
@@ -2,7 +2,7 @@
 #/#############################################################################
 #
 #    Tech-Receptives Solutions Pvt. Ltd.
-#    Copyright (C) 2009-TODAY Tech-Receptives(<http://www.tech-receptives.com>).
+#    Copyright (C) 2009-TODAY Tech-Receptives(<http://www.techreceptives.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
