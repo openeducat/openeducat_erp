1357c5a7c8f21f8e5652a5a2227687bdec2d551f
ajaychauhan727 <ajay.chauhan727@gmail.com>
2015-07-09 17:12:44 +0530

    [IMP] Change changelog file & version in openerp.py.

--- openeducat_erp/__openerp__.py
+++ openeducat_erp/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'OpenEduCat ERP',
-    'version': '1.0',
+    'version': '1.1.0',
     'category': 'Openerp Education',
     "sequence": 3,
     'summary': 'Manage Students, Faculties and Education Institute',
