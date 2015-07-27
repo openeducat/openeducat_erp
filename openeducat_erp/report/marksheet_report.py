7422e74bf0a802cdd5769313de629ae118624ad4
Siddharth <siddharth@techreceptives.com>
2015-07-27 15:37:04 +0530

    [Change] marksheetreport name change

--- openeducat_erp/report/marksheet_report.py
+++ openeducat_erp/report/marksheet_report.py
@@ -29,7 +29,7 @@ from openerp.report import report_sxw
 class MarksheetReport(report_sxw.rml_parse):
 
     def __init__(self, cr, uid, name, context=None):
-        super(marksheet_report, self).__init__(cr, uid, name, context=context)
+        super(MarksheetReport, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({
             'time': time,
             'get_lines': self.get_lines,
