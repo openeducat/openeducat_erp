99493fe3126d3dec7727d721e4b6643c8c940c4e
ajaychauhan727 <ajay.chauhan727@gmail.com>
2015-08-07 11:57:29 +0530

    [IMP] Added openeducat_assignment module.

---
+++ openeducat_assignment/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Tech-Receptives Solutions Pvt. Ltd.
+#    Copyright (C) 2009-TODAY Tech-Receptives(<http://www.techreceptives.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    'name': 'OpenEduCat Assignment',
+    'version': '2.0.0',
+    'category': 'Openerp Education',
+    'summary': 'Manage Assgiments',
+    'complexity': "easy",
+    'description': """
+        This module provide feature of Assignments.
+
+    """,
+    'author': 'Tech Receptives',
+    'website': 'http://www.openeducat.org',
+    'depends': ['openeducat_core'],
+    'data': [
+        'views/assignment_view.xml',
+        'views/assignment_sub_line_view.xml',
+        'assignment_menu.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
