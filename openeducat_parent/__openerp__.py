24d2bc9ffc1d2312999cf63c073312d7c3f5b3b5
Siddharth <siddharth@techreceptives.com>
2015-08-07 15:13:36 +0530

    [ADD] added openeducat_parent module

---
+++ openeducat_parent/__openerp__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Tech-Receptives Solutions Pvt. Ltd.
+#    Copyright (C) 2009-TODAY Tech-Receptives(<http://www.techreceptives.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    'name': 'OpenEduCat Parent',
+    'version': '1.1.0',
+    'category': 'Openerp Education',
+    "sequence": 3,
+    'summary': 'Manage Parent',
+    'complexity': "easy",
+    'description': """
+        This module provide parent management system over OpenERP
+    """,
+    'author': 'Tech Receptives',
+    'website': 'http://www.openeducat.org',
+    'depends': ['openeducat_core'],
+    'data': [
+        'security/op_parent_security.xml',
+        'views/parent_view.xml',
+        'parent_menu.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
