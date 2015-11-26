0e47ad3188a9111cfcb420d0f166251b81cf2716
siddharth-techreceptives <siddharth@techreceptives.com>
2015-11-26 15:55:01 +0530

    Flake8 fix

--- openeducat_exam/models/exam_session.py
+++ openeducat_exam/models/exam_session.py
@@ -19,8 +19,6 @@
 #
 ###############################################################################
 
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
 from openerp import models, fields, api, _
 from openerp.exceptions import ValidationError
 
