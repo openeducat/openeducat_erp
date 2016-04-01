87d49a6c12580f7e76f3b4d42a6c32410f9874f4
siddharth_techreceptives <siddharth@techreceptives.com>
2016-04-01 19:06:19 +0530

    [FIX] openeducat_support , file :  change old template name with new template name

---
+++ openeducat_support/static/src/js/user_menu.js
@@ -0,0 +1,15 @@
+odoo.define('web_user_menu', function (require) {
+"use strict";
+
+var UserMenu = require('web.UserMenu');
+
+UserMenu.include({
+    on_menu_documentation: function () {
+        window.open('http://doc.openeducat.org/', '_blank');
+    },
+    on_menu_support: function () {
+        window.open('https://www.openeducat.org/page/support', '_blank');
+    },
+});
+
+});
