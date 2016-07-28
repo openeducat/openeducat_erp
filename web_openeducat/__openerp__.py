4bae88876d40e2a02e383277081855944c46a16f
ajaychauhan727 <ajay.chauhan727@gmail.com>
2016-07-28 11:54:10 +0530

    [ADD] Added module web_openeducat & its depedency.

---
+++ web_openeducat/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Tech-Receptives Solutions Pvt. Ltd.
+#    Copyright (C) 2009-TODAY Tech-Receptives(<http://www.techreceptives.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    'name': 'Web OpenEduCat',
+    'description': 'Beautifies Website',
+    'category': 'Website',
+    "sequence": 3,
+    'version': '2.4.0',
+    'author': 'Tech Receptives',
+    'website': 'http://www.openeducat.org',
+    'data': [
+        'views/assets.xml',
+        'views/navbar_template.xml',
+    ],
+    'demo': [
+        'demo/footer_template.xml',
+        'demo/homepage.xml',
+        'demo/feature_template.xml',
+     ],
+    'depends': [
+        'website',
+        'theme_default',
+        'website_animate',
+    ],
+}
---
+++ website_animate/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Website Animate',
+    'description': "Provide animation for page\'s blocks",
+    'category': 'Website',
+    'version': '1.1',
+    'author': 'Odoo S.A.',
+    'data': [
+        'views/assets.xml',
+        'views/options.xml',
+    ],
+    'depends': ['website'],
+    'images': [
+        'static/description/icon.png',
+    ],
+}
