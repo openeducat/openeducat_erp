b369c8eb70badaadc65da76b08d61d82780ac3a0
Yusuf Kapasi <yusuf@techreceptives.com>
2018-10-10 11:23:55 +0530

    [ADD] Migrate Classroom in 12.0

---
+++ openeducat_classroom/models/asset.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Tech-Receptives Solutions Pvt. Ltd.
+#    Copyright (C) 2009-TODAY Tech-Receptives(<http://www.techreceptives.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+from odoo import models, fields
+
+
+class OpAsset(models.Model):
+    _name = "op.asset"
+    _description = "Classroom Assets"
+
+    asset_id = fields.Many2one('op.classroom', 'Asset')
+    product_id = fields.Many2one('product.product', 'Product', required=True)
+    code = fields.Char('Code', size=256)
+    product_uom_qty = fields.Float('Quantity', required=True)
