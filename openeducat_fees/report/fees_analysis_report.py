3b857c167ffc744b2c8fb53cb21ff04bc28e277a
Yusuf Kapasi <yusuf@techreceptives.com>
2018-10-11 12:08:23 +0530

    [ADD] Migrate Fees in 12.0

---
+++ openeducat_fees/report/fees_analysis_report.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Tech-Receptives Solutions Pvt. Ltd.
+#    Copyright (C) 2009-TODAY Tech-Receptives(<http://www.techreceptives.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+from odoo import models, api
+
+
+class ReportFeesAnalysis(models.AbstractModel):
+    _name = "report.openeducat_fees.report_fees_analysis"
+    _description = "Fees Analysis Report"
+
+    def get_total_amount(self, student_id):
+        total_amount = 0.0
+        for fees in student_id.fees_detail_ids:
+            total_amount += fees.amount
+        return total_amount
+
+    def get_total_left(self, student_id):
+        total_amount = self.get_total_amount(student_id)
+        total_paid = self.get_paid_amount(student_id)
+        return total_amount - total_paid
+
+    def get_paid_amount(self, student_id):
+        total_paid = 0.0
+        for fees in student_id.fees_detail_ids:
+            total_paid += fees.invoice_id.amount_total - fees.invoice_id.residual
+        return total_paid
+
+    @api.model
+    def get_report_values(self, docids, data=None):
+        student_ids = []
+        if data['fees_filter'] == 'student':
+            student_ids = self.env['op.student'].browse([data['student']])
+        else:
+            student_ids = self.env['op.student'].search(
+                [('course_detail_ids.course_id', '=', data['course'])])
+
+        docargs = {
+            'doc_ids': self.ids,
+            'doc_model': 'op.student',
+            'docs': student_ids,
+            'get_total_amount': self.get_total_amount,
+            'get_paid_amount': self.get_paid_amount,
+            'get_total_left': self.get_total_left,
+        }
+        return docargs
---
+++ openeducat_fees/wizard/fees_detail_report_wizard.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Tech-Receptives Solutions Pvt. Ltd.
+#    Copyright (C) 2009-TODAY Tech-Receptives(<http://www.techreceptives.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+from odoo import models, fields, api
+
+
+class FeesDetailReportWizard(models.TransientModel):
+
+    """ Admission Analysis Wizard """
+    _name = "fees.detail.report.wizard"
+    _description = "Wizard For Fees Details Report"
+
+    fees_filter = fields.Selection(
+        [('student', 'Student'), ('course', 'Course')],
+        'Fees Filter', required=True)
+    student_id = fields.Many2one('op.student', 'Student')
+    course_id = fields.Many2one('op.course', 'Course')
+
+    @api.multi
+    def print_report(self):
+        data = {}
+        if self.fees_filter == 'student':
+            data['fees_filter'] = self.fees_filter
+            data['student'] = self.student_id.id
+        else:
+            data['fees_filter'] = self.fees_filter
+            data['course'] = self.course_id.id
+
+        report = self.env.ref(
+            'openeducat_fees.action_report_fees_detail_analysis')
+        return report.report_action(self, data=data)
