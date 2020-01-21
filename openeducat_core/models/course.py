83dd0f716669018828766fddca729f041e0bd8b7
ShakirMemon786 <shakirmemon161@gmail.com>
2020-01-21 18:25:43 +0530

    [REM]: remove section field from course object

--- openeducat_core/models/course.py
+++ openeducat_core/models/course.py
@@ -30,7 +30,6 @@ class OpCourse(models.Model):
     name = fields.Char('Name', required=True)
     code = fields.Char('Code', size=16, required=True)
     parent_id = fields.Many2one('op.course', 'Parent Course')
-    section = fields.Char('Section', size=32, required=True)
     evaluation_type = fields.Selection(
         [('normal', 'Normal'), ('GPA', 'GPA'),
          ('CWA', 'CWA'), ('CCE', 'CCE')],
