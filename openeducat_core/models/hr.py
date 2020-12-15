0fad6da290f758678d3fea69f4261e5c1c088526
NIKUL AHIR <nikulahir90@gmail.com>
2020-12-15 10:03:34 +0530

    [Fix]hr view

--- openeducat_core/models/hr.py
+++ openeducat_core/models/hr.py
@@ -28,7 +28,6 @@ class HrEmployee(models.Model):
     @api.onchange('user_id')
     def onchange_user(self):
         if self.user_id:
-            self.user_id.partner_id.supplier = True
             self.work_email = self.user_id.email
             self.identification_id = False
 
