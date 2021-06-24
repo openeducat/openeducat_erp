8e3cbcd0465a19202c7716c92cea61e7c467ab41
srushti1000 <shrushti@techreceptives.com>
2021-06-24 18:49:25 +0530

    [Add][10.0] Add Company name convert

--- openeducat_timetable/__manifest__.py
+++ openeducat_timetable/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #
-#    Tech-Receptives Solutions Pvt. Ltd.
-#    Copyright (C) 2009-TODAY Tech-Receptives(<http://www.techreceptives.com>).
+#    OpenEduCat Inc.
+#    Copyright (C) 2009-TODAY OpenEduCat Inc(<http://www.openeducat.org>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Lesser General Public License as
@@ -27,7 +27,7 @@
     "sequence": 3,
     'summary': 'Manage TimeTables',
     'complexity': "easy",
-    'author': 'Tech Receptives',
+    'author': 'OpenEduCat Inc',
     'website': 'http://www.openeducat.org',
     'depends': ['openeducat_classroom'],
     'data': [
