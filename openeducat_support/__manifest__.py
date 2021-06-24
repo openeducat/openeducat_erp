dceec67e42ba03a0d6d46a839315330541452dbc
himanee <himanee@techreceptives.com>
2021-06-24 18:39:40 +0530

    [MODIFIED]11.0 replace the name of TechReceptives

--- openeducat_support/__manifest__.py
+++ openeducat_support/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #
-#    Tech-Receptives Solutions Pvt. Ltd.
-#    Copyright (C) 2009-TODAY Tech-Receptives(<http://www.techreceptives.com>).
+#    OpenEduCat Inc
+#    Copyright (C) 2009-TODAY OpenEduCat Inc(<http://www.openeducat.org>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Lesser General Public License as
@@ -25,7 +25,7 @@
     'version': '11.0.1.0.0',
     'license': 'LGPL-3',
     "sequence": 3,
-    'author': 'Tech Receptives',
+    'author': 'OpenEduCat Inc',
     'website': 'http://www.openeducat.org',
     'depends': ['web'],
     'data': [
