1c06a57989d06dbc14568adb59b2f0d06db45314
himanee <himanee@techreceptives.com>
2021-06-24 19:04:58 +0530

    [MODIFIED]8.0 change the organization name.

--- openeducat_erp/__openerp__.py
+++ openeducat_erp/__openerp__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #
-#    Tech-Receptives Solutions Pvt. Ltd.
-#    Copyright (C) 2009-TODAY Tech-Receptives(<http://www.techreceptives.com>).
+#    OpenEduCat Inc
+#    Copyright (C) 2009-TODAY OpenEduCat Inc(<http://www.openeducat.org>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -45,7 +45,7 @@
             * Hostel
 
     """,
-    'author': 'Tech Receptives',
+    'author': 'OpenEduCat Inc',
     'website': 'http://www.openeducat.org',
     'depends': ['account_accountant', 'document',
                 'hr', 'web', 'website'],
