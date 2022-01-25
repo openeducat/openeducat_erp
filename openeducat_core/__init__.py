8c704e2c6a8adaec8bd270b816d43c3d3e856930
Parthiv Patel <parthiv@techreceptives.com>
2022-01-25 10:41:41 +0000

    [FIX] update

--- openeducat_core/__init__.py
+++ openeducat_core/__init__.py
@@ -22,3 +22,18 @@
 from . import controllers
 from . import models
 from . import wizard
+
+
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import controllers
+from . import models
+from . import wizard
+from . import report
+
+from odoo import api, SUPERUSER_ID
+
+def _openeducat_post_init(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env['publisher_warranty.contract'].update_notification(cron_mode=True)
