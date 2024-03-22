31fc4c00df177e8922485d898fd318629cc12d6e
roshni_techreceptives <roshni.pujara111@gmail.com>
2024-03-22 11:10:43 +0530

    [MOD] Modify _get_modules_from_apps method.

--- openeducat_core/models/ir_module.py
+++ openeducat_core/models/ir_module.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
-import json
-import requests
 from odoo import api, fields, models, _
 from odoo.osv.expression import is_leaf
-from odoo.release import major_version
-from odoo.tools import file_open, file_open_temporary_directory, ormcache
 from odoo.exceptions import AccessDenied, UserError, ValidationError
 import logging
 
 _logger = logging.getLogger(__name__)
-
-APPS_URL = "https://apps.odoo.com"
 
 
 class IrModule(models.Model):
@@ -19,91 +13,42 @@ class IrModule(models.Model):
     module_type = fields.Selection(selection_add=[('institutes', 'Institutes')])
 
     @api.model
-    @ormcache('payload')
-    def _call_apps(self, payload):
-        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        return requests.post(
-            f"{APPS_URL}/loempia/listdatamodules",
-            data=payload,
-            headers=headers,
-            timeout=5.0,
-        )
-
-    @api.model
     def _get_modules_from_apps(self, fields, module_type, module_name, domain=None, limit=None, offset=None):
-        if 'name' not in fields:
-            fields = fields + ['name']
-        payload = {
-            'params': {
-                'series': major_version,
-                'module_fields': fields,
-                'module_type': module_type,
-                'module_name': module_name,
-                'domain': domain,
-                'limit': limit,
-                'offset': offset,
-            }
-        }
-        try:
-            resp = self._call_apps(json.dumps(payload))
-            resp.raise_for_status()
-            if module_type == 'institutes':
-                module_lst = []
-                all_module_lst = []
-                institutes_categories = self._get_institutes_categories_from_apps()
-                for data in institutes_categories:
-                    all_mod = self.env['ir.module.module'].search([('category_id.name', 'ilike', data)])
-                    for modules in all_mod:
-                        module_lst.append(modules)
-                for module in module_lst:
-                    values = {}
-                    values.update({
-                        'id': module.id,
-                        'summary': module.summary,
-                        'name': module.name,
-                        'icon': module.icon,
-                        'shortdesc': module.shortdesc,
-                        'website': module.website,
-                        'application': module.application,
-                        'module_type': 'institutes'
-                    })
-                    all_module_lst.append(values)
-                modules_list = all_module_lst
-                for mod in modules_list:
-                    module_name = mod['name']
-                    existing_mod = self.search([('name', '=', module_name), ('state', '=', 'installed')])
-                    if 'state' in fields:
-                        if existing_mod:
-                            mod['state'] = 'installed'
-                        else:
-                            mod['state'] = 'uninstalled'
-                    if 'module_type' in fields:
-                        mod['module_type'] = module_type
-                    if 'website' in fields:
-                        mod['website'] = f"{APPS_URL}/apps/modules/{major_version}/{module_name}/"
-                return modules_list
-            if module_type == 'industries':
-                modules_list = resp.json().get('result', [])
-                for mod in modules_list:
-                    module_name = mod['name']
-                    existing_mod = self.search([('name', '=', module_name), ('state', '=', 'installed')])
-                    mod['id'] = existing_mod.id if existing_mod else -1
-                    if 'icon' in fields:
-                        mod['icon'] = f"{APPS_URL}{mod['icon']}"
-                    if 'state' in fields:
-                        if existing_mod:
-                            mod['state'] = 'installed'
-                        else:
-                            mod['state'] = 'uninstalled'
-                    if 'module_type' in fields:
-                        mod['module_type'] = module_type
-                    if 'website' in fields:
-                        mod['website'] = f"{APPS_URL}/apps/modules/{major_version}/{module_name}/"
-                return modules_list
-        except requests.exceptions.HTTPError:
-            raise UserError(_('The list of industry applications cannot be fetched. Please try again later'))
-        except requests.exceptions.ConnectionError:
-            raise UserError(_('Connection to %s failed The list of industry modules cannot be fetched') % APPS_URL)
+        print(module_type, '.module_type')
+        if module_type == 'institutes':
+            module_lst = []
+            all_module_lst = []
+            institutes_categories = self._get_institutes_categories_from_apps()
+            for data in institutes_categories:
+                all_mod = self.env['ir.module.module'].search([('category_id.name', 'ilike', data)])
+                for modules in all_mod:
+                    module_lst.append(modules)
+            for module in module_lst:
+                values = {}
+                values.update({
+                    'id': module.id,
+                    'summary': module.summary,
+                    'name': module.name,
+                    'icon': module.icon,
+                    'shortdesc': module.shortdesc,
+                    'website': module.website,
+                    'application': module.application,
+                    'module_type': 'institutes'
+                })
+                all_module_lst.append(values)
+            modules_list = all_module_lst
+            for mod in modules_list:
+                module_name = mod['name']
+                existing_mod = self.search([('name', '=', module_name), ('state', '=', 'installed')])
+                if 'state' in fields:
+                    if existing_mod:
+                        mod['state'] = 'installed'
+                    else:
+                        mod['state'] = 'uninstalled'
+                if 'module_type' in fields:
+                    mod['module_type'] = module_type
+            return modules_list
+        return super()._get_modules_from_apps(fields, module_type, module_name, domain, limit, offset)
 
     @api.model
     def web_search_read(self, domain, specification, offset=0, limit=None, order=None, count_limit=None):
