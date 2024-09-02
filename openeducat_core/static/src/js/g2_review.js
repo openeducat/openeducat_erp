0d43d28e4cdc8a4be3a33d624e54525d80c1800b
jer.dewandre@gmail.com <jerome.dewandre.mail@gmail.com>
2024-09-02 16:41:28 +0200

    fix display bug

--- openeducat_core/static/src/js/g2_review.js
+++ openeducat_core/static/src/js/g2_review.js
@@ -12,33 +12,6 @@ $(document).ready(function(){
     const reviewShow = !!getCookie(`g2_review_${session.db}`);
 
     if(!reviewShow){
-        setTimeout(() => {
 
-            var review_dialog = new Dialog(null, {
-                title: 'G2 Review',
-                size: 'medium',
-                dialogClass: 'g2_review_dialog',
-                renderFooter: false,
-                renderHeader: false,
-                $parentNode: $(document.body),
-            });
-            review_dialog.open();
-            review_dialog.opened().then(function(){
-                review_dialog.$modal.find('.modal-dialog').addClass('modal-dialog-centered');
-                review_dialog.$modal.find('.modal-body').removeClass('modal-body');
-                review_dialog.$el.find('#write_g2_review').on('click', (e) => {
-                    e.preventDefault();
-                    window.open('https://openeducat.org/g2feedback', '_blank');
-                    review_dialog.close();
-
-                    setCookie(`g2_review_${session.db}`, true, nbDays * 24 * 60 * 60, 'required');
-                });
-                review_dialog.$el.find('.js_close_popup').on('click', (e) => {
-                    review_dialog.close();
-
-                    setCookie(`g2_review_${session.db}`, true, nbDays * 24 * 60 * 60, 'required');
-                });
-            });
-        }, 5000);
     }
 });
