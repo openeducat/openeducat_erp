eb824d6bdf9123debc84917cf767152fbfee9d35
jer.dewandre@gmail.com <jerome.dewandre.mail@gmail.com>
2024-09-11 11:14:59 +0200

    add search view to students

--- openeducat_core/models/student.py
+++ openeducat_core/models/student.py
@@ -130,8 +130,23 @@ class OpStudent(models.Model):
             'template': '/openeducat_core/static/xls/op_student.xls'
         }]
         
-    @api.model
     def create_student_user(self):
+        user_group = self.env.ref("base.group_portal") or False
+        users_res = self.env['res.users']
+        for record in self:
+            if not record.user_id:
+                user_id = users_res.create({
+                    'name': record.name,
+                    'partner_id': record.partner_id.id,
+                    'login': record.email,
+                    'groups_id': user_group,
+                    'is_student': True,
+                    'tz': self._context.get('tz'),
+                })
+                record.user_id = user_id
+                
+    @api.model      
+    def create_student_user_batch(self):
         # Use sudo() to bypass access rights checking if it's safe to do so
         self = self.sudo()
         
