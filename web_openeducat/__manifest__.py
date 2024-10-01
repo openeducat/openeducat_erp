aaa31798dddc04836d92f3472b8823ef81fe921f
sutharkamlesh <kamlesh@openeducat.com>
2024-10-01 13:03:53 +0530

    [18.0][MIG] erp module migrated

--- web_openeducat/__manifest__.py
+++ web_openeducat/__manifest__.py
@@ -23,7 +23,7 @@
     'name': 'Web OpenEduCat',
     'category': 'Website',
     "sequence": 3,
-    'version': '17.0.1.0',
+    'version': '18.0.1.0',
     'license': 'LGPL-3',
     'author': 'OpenEduCat Inc',
     'website': 'https://www.openeducat.org',
