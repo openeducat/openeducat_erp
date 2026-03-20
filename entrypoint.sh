c3e71e2f45bbd9793e5adb0d1252d3cf972ba452
gousswil <goussikindewilfried@yahoo.fr>
2026-03-20 17:02:49 +0000

    Use native Railway PG variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-echo "DB_HOST=$DB_HOST"
-echo "DB_PORT=$DB_PORT"
-echo "DB_NAME=$DB_NAME"
+echo "PGHOST=$PGHOST"
+echo "PGDATABASE=$PGDATABASE"
 exec python /odoo/odoo-bin \
-    --db_host="$DB_HOST" \
-    --db_port="$DB_PORT" \
-    --db_user="$DB_USER" \
-    --db_password="$DB_PASSWORD" \
-    --database="$DB_NAME" \
+    --db_host="$PGHOST" \
+    --db_port="$PGPORT" \
+    --db_user="$PGUSER" \
+    --db_password="$PGPASSWORD" \
+    --database="$PGDATABASE" \
     --addons-path=/odoo/addons,/mnt/extra-addons \
     --http-port="${PORT:-8069}" \
     -i base \
