299f2e21aa35104fa75193b0f8f8480ed932cdd5
gousswil <goussikindewilfried@yahoo.fr>
2026-03-20 17:37:33 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+cat > /workspaces/soome_erp/entrypoint.sh << 'EOF'
+#!/bin/bash
+exec python /odoo/odoo-bin \
+    --db_host=$DB_HOST \
+    --db_port=$DB_PORT \
+    --db_user=$DB_USER \
+    --db_password=$DB_PASSWORD \
+    --database=$DB_NAME \
+    --addons-path=/odoo/addons,/mnt/extra-addons \
+    --http-port=${PORT:-8069} \
+    -i base \
+    --without-demo=all
+EOF
+chmod +x /workspaces/soome_erp/entrypoint.sh
