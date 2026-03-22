2388b3b0d808c5a34b732ba3bd34904c8105eb0b
gousswil <goussikindewilfried@yahoo.fr>
2026-03-22 09:28:57 +0000

    Remove fixed database to allow multiple DBs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
-echo "PGHOST=$PGHOST"
-echo "PGDATABASE=$PGDATABASE"
 exec python /odoo/odoo-bin \
     --db_host="$PGHOST" \
     --db_port="$PGPORT" \
     --db_user="$PGUSER" \
     --db_password="$PGPASSWORD" \
-    --database="$PGDATABASE" \
     --addons-path=/odoo/addons,/mnt/extra-addons \
     --http-port="${PORT:-8069}" \
-    -i base \
     --without-demo=all
