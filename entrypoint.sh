52f3c3913e37a0799a1d19eaddc403038542a1a9
gousswil <goussikindewilfried@yahoo.fr>
2026-03-22 11:45:37 +0000

    Force single railway database with filter

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ exec python /odoo/odoo-bin \
     --db_port="$PGPORT" \
     --db_user="$PGUSER" \
     --db_password="$PGPASSWORD" \
+    --database=railway \
+    --db-filter="^railway$" \
     --addons-path=/odoo/addons,/mnt/extra-addons \
     --http-port="${PORT:-8069}" \
-    --without-demo=all
+    --without-demo=all \
+    -i base,web,mail,auth_totp,base_import,base_setup,bus,web_tour,iap,web_editor
