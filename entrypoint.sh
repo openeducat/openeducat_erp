d72d55a59baea07dc84b6b3634d65eff32051f20
gousswil <goussikindewilfried@yahoo.fr>
2026-03-22 10:07:44 +0000

    Fix: initialize all required modules on startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ exec python /odoo/odoo-bin \
     --db_port="$PGPORT" \
     --db_user="$PGUSER" \
     --db_password="$PGPASSWORD" \
+    --database="$PGDATABASE" \
     --addons-path=/odoo/addons,/mnt/extra-addons \
     --http-port="${PORT:-8069}" \
     --without-demo=all \
-    --load=base,web
+    -i base,web,mail,auth_totp,base_import,base_setup,bus,web_tour,iap,web_editor
