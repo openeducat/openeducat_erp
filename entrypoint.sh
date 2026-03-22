bdc2cc29afc226b407e3875fa4e2695f4e20d71e
gousswil <goussikindewilfried@yahoo.fr>
2026-03-22 10:32:03 +0000

    Remove fixed database parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,6 @@ exec python /odoo/odoo-bin \
     --db_port="$PGPORT" \
     --db_user="$PGUSER" \
     --db_password="$PGPASSWORD" \
-    --database="$PGDATABASE" \
     --addons-path=/odoo/addons,/mnt/extra-addons \
     --http-port="${PORT:-8069}" \
-    --without-demo=all \
-    -i base,web,mail,auth_totp,base_import,base_setup,bus,web_tour,iap,web_editor
+    --without-demo=all
