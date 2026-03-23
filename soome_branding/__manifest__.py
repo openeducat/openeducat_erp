1fd64585be8afeaa25a7dbf5c4834d06ade5c057
gousswil <goussikindewilfried@yahoo.fr>
2026-03-23 17:15:22 +0000

    Add SOOME logo to database selector page

--- soome_branding/__manifest__.py
+++ soome_branding/__manifest__.py
@@ -2,10 +2,16 @@
     'name': 'SOOME Branding',
     'version': '17.0.1.0.0',
     'summary': 'Rebranding OpenEduCat vers SOOME',
-    'depends': ['openeducat_core'],
+    'depends': ['openeducat_core', 'web'],
     'data': [
         'data/ir_ui_menu.xml',
+        'views/database_selector.xml',
     ],
+    'assets': {
+        'web.assets_common': [
+            'soome_branding/static/src/img/soome_logo.svg',
+        ],
+    },
     'post_init_hook': 'post_init_hook',
     'installable': True,
     'auto_install': True,
