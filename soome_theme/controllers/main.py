9f3b3fbaec8fd2e8f0b0b298ee87c6640e30f0a1
gousswil <goussikindewilfried@yahoo.fr>
2026-03-23 17:33:30 +0000

    Use PNG logo in controller

--- soome_theme/controllers/main.py
+++ soome_theme/controllers/main.py
@@ -6,13 +6,11 @@ class SoomeTheme(http.Controller):
 
     @http.route('/web/binary/company_logo', type='http', auth='none')
     def company_logo(self, dbname=None, **kwargs):
-        # Si pas de base, retourner le logo SOOME
         if not dbname:
             logo_path = os.path.join(
                 os.path.dirname(os.path.dirname(__file__)),
-                'static', 'src', 'img', 'soome_logo.svg'
+                'static', 'src', 'img', 'soome_logo.png'
             )
             with open(logo_path, 'rb') as f:
-                return Response(f.read(), content_type='image/svg+xml')
-        # Sinon comportement normal d'Odoo
+                return Response(f.read(), content_type='image/png')
         return request.redirect(f'/web/binary/company_logo?dbname={dbname}')
