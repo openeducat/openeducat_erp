06ba09154b7f8aa6fd67b248cc49f35650633c8e
gousswil <goussikindewilfried@yahoo.fr>
2026-03-23 18:06:27 +0000

    Add green CSS to soome_theme

--- soome_theme/__manifest__.py
+++ soome_theme/__manifest__.py
@@ -1,11 +1,16 @@
 {
     'name': 'SOOME Theme',
     'version': '17.0.1.0.0',
-    'summary': 'Theme global SOOME - logo et apparence',
+    'summary': 'Theme global SOOME',
     'depends': ['web'],
     'data': [
         'views/login_layout.xml',
     ],
+    'assets': {
+        'web.assets_common': [
+            'soome_theme/static/src/css/soome.css',
+        ],
+    },
     'installable': True,
     'auto_install': False,
 }
