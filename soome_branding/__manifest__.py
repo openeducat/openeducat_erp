30df0c13f5311b7ec2d037f255e869df435a5de4
gousswil <goussikindewilfried@yahoo.fr>
2026-03-23 10:34:30 +0000

    Add SOOME branding module

---
+++ soome_branding/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'SOOME Branding',
+    'version': '17.0.1.0.0',
+    'summary': 'Rebranding OpenEduCat vers SOOME',
+    'depends': ['openeducat_core'],
+    'data': [
+        'data/ir_ui_menu.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
