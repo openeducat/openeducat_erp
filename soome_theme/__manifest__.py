3414254ca5f8649f15423b1759f8dd50faf94ebb
gousswil <goussikindewilfried@yahoo.fr>
2026-03-23 17:26:55 +0000

    Add soome_theme module for global branding

---
+++ soome_theme/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'SOOME Theme',
+    'version': '17.0.1.0.0',
+    'summary': 'Theme global SOOME - logo et apparence',
+    'depends': ['web'],
+    'data': [
+        'views/login_layout.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
