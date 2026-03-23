4182ac0621732d8868f1538d13371f7895475965
gousswil <goussikindewilfried@yahoo.fr>
2026-03-23 10:51:45 +0000

    Add permanent branding in post_init_hook

--- soome_branding/models/res_company.py
+++ soome_branding/models/res_company.py
@@ -1,6 +1,28 @@
 from odoo import api, SUPERUSER_ID
 
+
 def post_init_hook(env):
-    menus = env['ir.ui.menu'].search([('name', 'like', 'OpenEduCat')])
-    for menu in menus:
-        menu.name = menu.name.replace('OpenEduCat', 'SOOME')
+    # Renommer les modules OpenEduCat -> SOOME
+    modules = env['ir.module.module'].search([('name', 'like', 'openeducat')])
+    for m in modules:
+        if 'OpenEduCat' in (m.shortdesc or ''):
+            m.shortdesc = m.shortdesc.replace('OpenEduCat', 'SOOME')
+
+    # Renommer les menus racines
+    renommages = {
+        'menu_op_school_root': 'SOOME',
+        'menu_op_faculty_root': 'SOOME - Enseignants',
+        'menu_openeducat_config': 'SOOME - Paramètres',
+    }
+    data = env['ir.model.data'].search([
+        ('module', 'like', 'openeducat'),
+        ('model', '=', 'ir.ui.menu'),
+        ('name', 'in', list(renommages.keys()))
+    ])
+    for d in data:
+        menu = env['ir.ui.menu'].browse(d.res_id)
+        nouveau_nom = renommages.get(d.name)
+        if nouveau_nom:
+            menu.name = nouveau_nom
+
+    env.cr.commit()
