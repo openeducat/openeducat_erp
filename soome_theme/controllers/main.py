f949decd0da0312044a64f2a7257680cf984c8e0
gousswil <goussikindewilfried@yahoo.fr>
2026-03-23 17:29:12 +0000

    Override company_logo route for SOOME branding

---
+++ soome_theme/controllers/main.py
@@ -0,0 +1,18 @@
+from odoo import http
+from odoo.http import request, Response
+import os
+
+class SoomeTheme(http.Controller):
+
+    @http.route('/web/binary/company_logo', type='http', auth='none')
+    def company_logo(self, dbname=None, **kwargs):
+        # Si pas de base, retourner le logo SOOME
+        if not dbname:
+            logo_path = os.path.join(
+                os.path.dirname(os.path.dirname(__file__)),
+                'static', 'src', 'img', 'soome_logo.svg'
+            )
+            with open(logo_path, 'rb') as f:
+                return Response(f.read(), content_type='image/svg+xml')
+        # Sinon comportement normal d'Odoo
+        return request.redirect(f'/web/binary/company_logo?dbname={dbname}')
